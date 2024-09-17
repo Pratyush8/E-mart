dbcfd5287de4a4b994ad5722fe58c5df9cd7dd42
pratyush <pratyush.saxena@tothenew.com>
2024-09-17 14:01:52 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./component/navbar/Navbar";
+import Products from "./component/products/Products";
+import Footer from "./component/footer/Footer";
+import Home from "./component/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductDetail from "./component/productdetail/ProductDetail";
+import Cart from "./component/cart/Cart";
+import OrderPlaced from "./component/orderplaced/OrderPlaced";
+import Payment from "./component/cart/Payment";
+import ShippingDetails from "./component/cart/ShippingDetails";
+import ShoppingCart from "./component/cart/ShoppingCart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/orderplaced" component={OrderPlaced} />
+        <Route path="/shipping" component={ShippingDetails} />
+        <Route path="/shopping" component={ShoppingCart} />
+        <Route path="/payment" component={Payment} />
+        <Route exact path="/products/:id" component={ProductDetail} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
